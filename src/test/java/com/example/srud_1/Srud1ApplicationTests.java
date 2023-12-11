c0e2e6d3fa48aa763bac4f56f8e42abe48a69be5
lt460 <130529289+lt460@users.noreply.github.com>
2023-12-11 20:21:56 +0600

    Start

---
+++ src/test/java/com/example/srud_1/Srud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srud_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Srud1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
